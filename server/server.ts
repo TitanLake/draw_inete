81a6ba3e93e2a8a3a4f87a562c2af3b902f4a261
I200040Russo <i200040@inete.net>
2023-02-17 08:57:49 +0000

    add static folder to client side

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,30 @@
 import path from "path";
 
 const app = require('express')();
+import express from "express";
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
 import {Socket} from "socket.io"
 import {Request,Response} from "express";
 
+var htmlPath = path.resolve("client");
+app.use(express.static(htmlPath));
+
+interface ISocketToUser
+{
+  [key:string]:{room_id:string,user_name:string}
+}
 
 io.on("connection",(socket:Socket)=>{
 
     console.log(Socket);
     
-    
-
 })
 
 app.get('/', function(req:Request, res:Response) {
+
+
 
   return res.sendfile(path.resolve("client","index.html"));
   
